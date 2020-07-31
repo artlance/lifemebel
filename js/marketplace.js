63b43cce743c0b13aba919e7f1626fca81cbd31a
artlance <artlance.ru@gmail.com>
2020-07-31 20:17:50 +0300

    marketplace 3

--- js/marketplace.js
+++ js/marketplace.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+    //nojs
+    $('body').removeClass('no-js');
+
+    //------------------------------------------------------------------------//
+
+    //fakelink
+    $(document).on('click', 'a[href="#"]', function(event) {
+        event.preventDefault();
+    });
+
+    //------------------------------------------------------------------------//
+
     //marketplace document
     $(document).on('change', '.marketplace-document-upload input', function(event) {
         var thisValue = $(this).val(),
